50ed50a855e3e1c426ae5ddcd08dcdc0688bd920
ghifjdkcqlm <shivlidimri652@gmail.com>
2024-10-24 06:27:13 +0530

    Create q1.java

---
+++ lab 10/q1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class q1 {
+    public static void main(String[] args){
+        try{
+            System.out.println(Integer.parseInt("s"));
+        }
+        catch(NumberFormatException e){
+            System.out.println(e);
+        }
+    }
+    
+}
