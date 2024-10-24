3d4c6ca70a3dec9f064e5ab5f3d8bf10b8e7bdc5
ghifjdkcqlm <shivlidimri652@gmail.com>
2024-10-24 06:28:52 +0530

    Create q2.java

---
+++ lab 10/q2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class q1 {
+    public static void main(String[] args){
+        int arr[] = new int[9]; 
+        try{
+
+System.out.println(arr[11]);
+
+        }
+        catch(ArrayIndexOutOfBoundsException r){
+            System.out.println(r);
+        }
+    }
+    
+}
